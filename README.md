db4e4419a1805f1ae83ab38be8405bfc904cde4b
sakamotoyuya <freetairu@yahoo.co.jp>
2021-01-21 06:23:05 +0900

    ディレクトリ構成の整理
    ・テンプレートファイル(_preview.hbs)を追加
    ・gulpfile.jsを整理
    ・README.mdを整理
    ・全体のディレクトリ構成を見直し

--- README.md
+++ README.md
@@ -8,26 +8,16 @@
 - fractal
 
 ## require
-node.js
-gulp
+[node.js](https://nodejs.org/ja/)
+[gulp.js](https://gulpjs.com/)
 
 ## 実行方法
 ```
 npm install
 gulp
 ```
-## 備忘録
-以下のサイトを参考に本プロジェクトを作成。
-- [参考サイト１](https://qiita.com/nabe_kurage/items/b3f154a09962f692df14)
-- [参考サイト2](https://www.npmjs.com/package/gulp-autoprefixer)
-- [参考サイト3](https://webdesign-trends.net/entry/10069)
-- [参考サイト4](https://3owebcreate.com/web/coding/webpack_autoprefixer_setting)
-- [参考サイト5](https://satoyan419.com/gulp-v4/)
-- [参考サイト6](https://codecodeweb.com/blog/459/)
-- [参考サイト7](https://www.tweeeety.blog/entry/2018/06/18/060030)
-
-必要なプラグインのインストール
-コマンドライン
+## 環境構築
+以下コマンドでpackage.jsonを作成
 ```
 npm init
 npm install gulp --save-dev
@@ -36,145 +26,78 @@ npm install browser-sync --save-dev
 npm install gulp-plumber --save-dev
 npm install --save-dev gulp-autoprefixer
 npm install @frctl/fractal --save-dev
-
 ```
+## scssのビルドについて
+- ビルド元のscss
+   - `web/sass`
+- scssビルド時の出力先
+   - `web/css`
+- scssを出力後はstyleguideの静的ディレクトリにコピーされる
+  - `web/css`と`web/sass`を`/styleguide/assets`にコピー 
 
-- glupfile.js
-```js
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var browser = require("browser-sync");
-var autoprefixer = require("gulp-autoprefixer");
-var plumber = require("gulp-plumber");
-const fractal = require('@frctl/fractal').create();
-
-fractal.set('project.title', 'FooCorp Component Library'); // title for the project
-fractal.web.set('builder.dest', `${__dirname}/styleguide/build`); // destination for the static export
-fractal.docs.set('path', `${__dirname}/styleguide/docs`); // location of the documentation directory.
-fractal.components.set('path', `${__dirname}/styleguide/components`); // location of the component directory.
-/*
- * Tell the Fractal web preview plugin where to look for static assets.
- * webの場所を設定
- */
-fractal.web.set('static.path', __dirname + '/styleguide/public');
-
-// any other configuration or customisation here
-
-const logger = fractal.cli.console; // keep a reference to the fractal CLI console utility
-
-/*
- * Start the Fractal server
- *
- * In this example we are passing the option 'sync: true' which means that it will
- * use BrowserSync to watch for changes to the filesystem and refresh the browser automatically.
- * Obviously this is completely optional!
- *
- * This task will also log any errors to the console.
- */
-
-gulp.task('fractal:start', function(){
-    const server = fractal.web.server({
-        sync: true
-    });
-    server.on('error', err => logger.error(err.message));
-    return server.start().then(() => {
-        logger.success(`Fractal server is now running at ${server.url}`);
-    });
-});
-
-/*
- * Run a static export of the project web UI.
- *
- * This task will report on progress using the 'progress' event emitted by the
- * builder instance, and log any errors to the terminal.
- *
- * The build destination will be the directory specified in the 'builder.dest'
- * configuration option set above.
- */
-
-gulp.task('fractal:build', function(){
-    const builder = fractal.web.builder();
-    builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
-    builder.on('error', err => logger.error(err.message));
-    return builder.build().then(() => {
-        logger.success('Fractal build completed!');
-    });
-});
-
-gulp.task("server", function(db) {
-  browser({
-    server: {
-      baseDir: "./"//ドキュメントディレクトリを指定する。
-    }
-  });
-  db();
-});
-
-gulp.task("sass",function(db){//タスクの登録（"sass"タスク登録)
-  gulp.src("sass/**/*.scss")//gulp.src()...読み込むファイルの設定
-  .pipe(plumber())//plumber()...エラーが起きたとしても強制終了させない
-  .pipe(sass({outputStyle:'expanded'}))
-  //pipe()...srcで取得したファイルに行う処理を記載
-  //sass()...コンパイルの実行をする。outputStyleで吐き出すcssのスタイルを設定
-  .pipe(autoprefixer({//autoprefixer()...ベンダープレフィックスの付与
-    cascade:false
-  }))
-  .pipe(gulp.dest("./css"))//gulp.dest()...出力したい場所を記載
-  // .pipe(gulp.dest("./styleguide/public/css"))//gulp.dest()...出力したい場所を記載
-  // .pipe(gulp.dest("./styleguide/components/example"));
-
-  gulp.src('./css/button.css')//ルートフォルダのbutton.cssを
-      .pipe(gulp.dest('./styleguide/public/css/'))//styleguide/public/cssにコピーする
-      .pipe(gulp.dest('./styleguide/components/button/'));//styleguide/components/buttonにコピーする
-  db();
-});
-
-gulp.task("reload",function(db){
-  browser.reload();//ブラウザーのリロード
-  db();
-})
-
-gulp.task("watch",function(db){
-  //watch(['監視するファイル'],['実行したいタスク名'])
-  //タスク名をdefaultにするとコマンドにタスク名を入れる必要がなくなる。
-
-  //scssファイルに更新があったらsassタスク、reloadタスクを実行する
-  gulp.watch("sass/**/*.scss",gulp.parallel(["sass","reload"]));
-  //htmlファイルに更新があったらreloadタスクを実行する
-  gulp.watch("./**/*.html",gulp.series(["reload"]));
-  db();
-});
-
-//sassタスクwatchタスク、serverタスクを実行する。
-// gulp.task('default',gulp.series(gulp.parallel(['sass','watch','server'])));
-gulp.task('default',gulp.series(gulp.parallel(['fractal:start','fractal:build','sass','watch','server'])));
-
-```
-package.jsonの修正
-```json
-{
-  "name": "omikuji",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "author": "",
-  "license": "ISC",
-  "devDependencies": {
-    "@frctl/fractal": "^1.5.1",
-    "browser-sync": "^2.26.7",
-    "gulp": "^4.0.2",
-    "gulp-autoprefixer": "^7.0.1",
-    "gulp-plumber": "^1.2.1",
-    "gulp-sass": "^4.1.0"
-  },
-  //★ここにautoprefixerの設定を追加
-  "browserslist": [
-    "last 2 version",
-    "> 5%",
-    "ie >= 9"
-  ]
-}
-```
+## ディレクトリの説明
+- node_modules
+  - `npm install`で取得したパッケージ
+- styleguide
+  - スタイルガイド(fractal)
+- styleguide/assets
+  - 静的ディレクトリ
+    `web/css`、`web/sass`からコピーされて生成される  
+    静的ディレクトリはgulpfile.jsで指定
+  - styleguideはこのディレクトリをルートパスとして`{{ path '/css/button.css' }}`のように指定可能
+- styleguide/build
+  - スタイルガイドの静的WebUIディレクトリ　　
+    gulp処理中の`fractal:build`にて本ディレクトリが生成される
+- styleguide/components
+  - スタイルガイドの生成元の構成ディレクトリ
+  - `_preview.hbs`がテンプレートファイル
+- styleguide/docs
+  - スタイルガイドのドキュメントディレクトリ
+  - ドキュメントをマークダウンで残す
+- web
+  - おみくじアプリ
+- web/sass
+  - ビルド元のscss
+- web/css
+  - web/sassから生成される
+- gulpfile.js
+  - gulp設定ファイル
+    `gulp`コマンド実行時に動作させる処理を記載する
+- package.json
+  - npmで導入するパッケージの設定
+  ```json
+  {
+    "name": "omikuji",
+    "version": "1.0.0",
+    "description": "",
+    "main": "index.js",
+    "scripts": {
+      "test": "echo \"Error: no test  specified\" && exit 1"
+    },
+    "author": "",
+    "license": "ISC",
+    "devDependencies": {
+      "@frctl/fractal": "^1.5.1",
+      "browser-sync": "^2.26.7",
+      "gulp": "^4.0.2",
+      "gulp-autoprefixer": "^7.0.1",
+      "gulp-plumber": "^1.2.1",
+      "gulp-sass": "^4.1.0"
+    },
+    //★ここにautoprefixerの設定を追加
+    "browserslist": [
+      "last 2 version",
+      "> 5%",
+      "ie >= 9"
+    ]
+  }
+  ```
+## 備忘録
+以下のサイトを参考に本プロジェクトを作成。
+- [参考サイト1](https://qiita.com/nabe_kurage/items/b3f154a09962f692df14)
+- [参考サイト2](https://www.npmjs.com/package/gulp-autoprefixer)
+- [参考サイト3](https://webdesign-trends.net/entry/10069)
+- [参考サイト4](https://3owebcreate.com/web/coding/webpack_autoprefixer_setting)
+- [参考サイト5](https://satoyan419.com/gulp-v4/)
+- [参考サイト6](https://codecodeweb.com/blog/459/)
+- [参考サイト7](https://www.tweeeety.blog/entry/2018/06/18/060030)
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,69 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var browser = require("browser-sync");
-var autoprefixer = require("gulp-autoprefixer");
-var plumber = require("gulp-plumber");
-const fractal = require('@frctl/fractal').create();
-const port_fractal = 3000;//フラクタルサーバポート
-const port_server = 4000;//アプリサーバポート
-const port_ui = 5000;//アプリUIポート
-fractal.set('project.title', 'おみくじアプリのスタイルガイド'); // title for the project
-fractal.web.set('builder.dest', `${__dirname}/styleguide/build`); // destination for the static export
-fractal.docs.set('path', `${__dirname}/styleguide/docs`); // location of the documentation directory.
-fractal.components.set('path', `${__dirname}/styleguide/components`); // location of the component directory.
-/*
- * Tell the Fractal web preview plugin where to look for static assets.
- * webの場所を設定
+/**
+ * ライブラリインポート
  */
-fractal.web.set('static.path', __dirname + '/styleguide/public');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browser = require("browser-sync");
+const autoprefixer = require("gulp-autoprefixer");
+const plumber = require("gulp-plumber");
+const fractal = require('@frctl/fractal').create();
+const logger = fractal.cli.console;    // keep a reference to the fractal CLI console utility
 
-// fractal.web.set('server.port',8080);//fractalサーバのポート設定
+/**
+ * 定数
+ */
+const port_fractal = 3000;                                            // フラクタルサーバポート
+const port_server = 4000;                                             // アプリサーバポート
+const port_ui = 5000;                                                 // アプリUIポート
+const dir_assets = __dirname + "/styleguide/assets"                   // 静的ファイルディレクトリ
+const dir_build = __dirname + "/styleguide/build";                    // 静的UI書出先ディレクトリ
+const dir_docs = __dirname + "/styleguide/docs";                      // docsディレクトリ
+const dir_components = __dirname + "/styleguide/components";          // componentsディレクトリ
+const dir_app = __dirname + "/web";                                   // アプリディレクトリ
+const dir_sass = dir_app + "/sass/**/*.scss";                         // sassのコンパイルディレクトリ
+const project_title = "おみくじアプリのスタイルガイド";                    // アプリタイトル
+/**
+ * gulpタスク
+ */
+const task_fractal_start = "fractal:start";                           // flactalサーバを起動する
+const task_fractal_build = "fractal:build";                           // fractalのビルドを行う
+const task_sass = "sass";                                             // sassのビルドを行う
+const task_watch = "watch";                                           // ディレクトリを監視し変更契機にビルドを行う
+const task_server = "server";                                         // おみくじアプリサーバを起動する
+const task_reload = "reload";                                         // ブラウザをリロードする
+const task_default = "default";                                       // default
+
+/**
+ * defaultタスクで実行するタスク
+ */
+const start_task = [
+  task_fractal_start,
+  task_sass,
+  task_fractal_build,
+  task_watch,
+  task_server
+];
+
+/**
+ * フラクタル設定
+ */
+fractal.web.set('builder.dest', dir_build);                           // 静的UI書出先ディレクトリ
+fractal.set('project.title', project_title);                          // プロジェクトタイトル
+fractal.web.set('static.path', dir_assets);                           // 静的ファイルディレクトリ
+fractal.docs.set('path', dir_docs);                                   // docsディレクトリ
+fractal.components.set('path', dir_components);                       // componentディレクトリ
+// fractal.web.set('server.port',8080);                               //fractalサーバのポート設定
 // fractal.web.set('server.syncOptions',{
 //   open:true,
-//   browser:['chrome'],//ブラウザを何で開くか指定
-//   notify:true,//開けなかったときの通知を出すか指定
-//   ui:{port:11111}//ポート指定
+//   browser:['chrome'],                                              //ブラウザを何で開くか指定
+//   notify:true,                                                     //開けなかったときの通知を出すか指定
+//   ui:{port:11111}                                                  //ポート指定
 // });
 
-// any other configuration or customisation here
 
-const logger = fractal.cli.console; // keep a reference to the fractal CLI console utility
+/**
+ * 実行タスク
+ */
 
 /*
  * Start the Fractal server
@@ -38,18 +74,16 @@ const logger = fractal.cli.console; // keep a reference to the fractal CLI conso
  *
  * This task will also log any errors to the console.
  */
-
-gulp.task('fractal:start', function(db){
+gulp.task(task_fractal_start, function(db){
     const server = fractal.web.server({
         sync: true,
-        port:port_fractal//fractalサーバのポート設定
+        port:port_fractal                               //fractalサーバのポート設定
     });
     server.on('error', err => logger.error(err.message));
     return server.start().then(() => {
         logger.success(`Fractal server is now running at ${server.url}`);
     });
     db();
-
 });
 
 /*
@@ -61,77 +95,98 @@ gulp.task('fractal:start', function(db){
  * The build destination will be the directory specified in the 'builder.dest'
  * configuration option set above.
  */
-
-gulp.task('fractal:build', function(db){
+gulp.task(task_fractal_build, function(db){
     const builder = fractal.web.builder();
-    builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
-    builder.on('error', err => logger.error(err.message));
+    builder.on('progress', (completed, total) => {
+      logger.update(`Exported ${completed} of ${total} items`, 'info')
+    });
+    builder.on('error', err => logger.error(err.message));    
+    db();
     return builder.start().then(() => {
       logger.success('Fractal build completed!');
     });
-    db();
     // return builder.build().then(() => {
     //     logger.success('Fractal build completed!');
     // });
-
 });
 
-gulp.task("server", function(db) {
+/**
+ * serverタスク
+ * defaultタスクから呼び出されるタスク
+ */
+gulp.task(task_server, function(db) {
   browser({
-    server: {
-      baseDir: "./web/"//ドキュメントディレクトリを指定する。
-    },
-    port:port_server,//サーバポート設定
-    ui:{
-      port:port_ui//UIポート設定
-    },
+    server: {baseDir: dir_app},          // アプリサーバの先頭アクセス時ディレクトリ設定
+    port:port_server,                     // アプリサーバのポート設定
+    ui: {port:port_ui}                    //UIポート設定
   });
   db();
 });
 
-const publicfolder = "./styleguide/public/css/";
-const componentsfolder = "./styleguide/components/";
+/**
+ * sassタスク
+ * defaultタスクから呼び出されるタスク
+ * 
+ */
+gulp.task(task_sass,function(db){
+  /* sassのコンパイル(./web/sass → ./web/css) */
+  gulp.src(dir_sass)                        //gulp.src()...読み込むファイルの設定
+    .pipe(plumber())                        //plumber()...エラーが起きたとしても強制終了させない
+    .pipe(sass({outputStyle:'expanded'}))   //sassからcssを圧縮して出力する
+                                            //pipe()...srcで取得したファイルに行う処理を記載
+                                            //sass()...コンパイルの実行をする。outputStyleで吐き出すcssのスタイルを設定
+    .pipe(autoprefixer({                    //autoprefixer()...ベンダープレフィックスの付与
+      cascade:false
+    }))
+    .pipe(gulp.dest("./web/css/"));         //gulp.dest()...出力したい場所を記載
 
-gulp.task("sass",function(db){//タスクの登録（"sass"タスク登録)
-  gulp.src("./web/sass/**/*.scss")//gulp.src()...読み込むファイルの設定
-  .pipe(plumber())//plumber()...エラーが起きたとしても強制終了させない
-  .pipe(sass({outputStyle:'expanded'}))
-  //pipe()...srcで取得したファイルに行う処理を記載
-  //sass()...コンパイルの実行をする。outputStyleで吐き出すcssのスタイルを設定
-  .pipe(autoprefixer({//autoprefixer()...ベンダープレフィックスの付与
-    cascade:false
-  }))
-  .pipe(gulp.dest("./web/css/"))//gulp.dest()...出力したい場所を記載
-  .pipe(gulp.dest(publicfolder));//gulp.dest()...出力したい場所を記載
- 
-  //./css/button.cssを別のフォルダに書き出す
-  const items = ["button","layout","form","table","common","card","style"];
-  // const items = ["button"];
-  items.forEach(function (item){
-    gulp.src('./web/css/' + item + '.css')
-    .pipe(gulp.dest(publicfolder))
-    .pipe(gulp.dest(componentsfolder + item));
-  });
+  /* ディレクトリごとコピー(./web/css/** → ./assets/css) */
+  gulp.src(__dirname + "/web/css/**")
+    .pipe(gulp.dest(dir_assets + "/css"));
+
+  /* ディレクトリごとコピー()./web/sass/** → ./assets/sass) */
+  gulp.src(__dirname + "/web/sass/**")
+    .pipe(gulp.dest(dir_assets + "/sass"));
+
+  // const items = ["button","layout","form","table","common","card","style"];
+  // items.forEach(function (item){
+  //   gulp.src('./web/css/' + item + '.css')
+  //   .pipe(gulp.dest(dir_assets + "/css"))
+  //   .pipe(gulp.dest(dir_components + "/" + item));
+  // });
   db();
 });
 
-gulp.task("reload",function(db){
-  browser.reload();//ブラウザーのリロード
+/**
+ * reloadタスク
+ * watchタスクから呼び出されるタスク
+ * ブラウザをリロードする
+ */
+gulp.task(task_reload,function(db){
+  browser.reload();
   db();
 })
 
-gulp.task("watch",function(db){
-  //watch(['監視するファイル'],['実行したいタスク名'])
-  //タスク名をdefaultにするとコマンドにタスク名を入れる必要がなくなる。
-
-  //scssファイルに更新があったらsassタスク、reloadタスクを実行する
-  gulp.watch("./web/sass/**/*.scss",gulp.parallel(["sass","reload"]));
-  //htmlファイルに更新があったらreloadタスクを実行する
-  gulp.watch(["./**/*.html","./**/*.hbs"],gulp.series(["reload"]));
+/**
+ * watchタスク
+ * defaultタスクから呼び出されるタスク
+ * scssファイルに更新があったらsassタスク、reloadタスクを実行する
+ * htmlファイルに更新があったらreloadタスクを実行する
+ */
+gulp.task(task_watch,function(db){
+  // gulp.watch(監視するファイルパス,gulp.parallel([実行タスク名]));・・・並列実行
+  gulp.watch("./web/sass/**/*.scss",gulp.parallel([task_sass,task_reload]));//※並列実行の根拠なし直列で動くと思う
+  // gulp.watch(監視するファイルパス,gulp.series([実行タスク名]));・・・直列実行
+  gulp.watch(["./**/*.html","./**/*.hbs"],gulp.series([task_reload]));
   db();
 });
 
-//sassタスクwatchタスク、serverタスクを実行する。
+/**
+ * defaultタスク
+ * `gulp`コマンドで動作させるタスク
+ * 本来gulpのcli実行コマンドは、gulp.task(タスク名,処理)"gulp タスク名"というように実行する必要があるが、
+ * タスク名をdefaultにするとコマンドにタスク名を入れる必要がなくなる。
+ */
+gulp.task(task_default,gulp.series(start_task));
 // gulp.task('default',gulp.series(gulp.parallel(['sass','watch','server'])));
 // gulp.task('default',gulp.series(gulp.parallel(['fractal:start','sass','fractal:build','watch','server'])));
-gulp.task('default',gulp.series(['fractal:start','sass','fractal:build','watch','server']));
