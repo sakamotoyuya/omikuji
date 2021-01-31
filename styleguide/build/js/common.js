9eae772553be9ea65bf58c9718b559ce7f3867ad
sakamotoyuya <freetairu@yahoo.co.jp>
2021-01-31 13:00:09 +0900

    headerデザインを微調整

--- styleguide/build/js/common.js
+++ styleguide/build/js/common.js
@@ -1,17 +1,9 @@
-$('.nav1-btn,.nav1-bg').on('click',function(){
-    if($('.nav1-menu').hasClass('is-active')){
-        $('.nav1-menu').removeClass('is-active');
-        $('.nav1-btn').removeClass('is-active');
+$('.nav-btn,.nav-bg').on('click',function(){
+    if($('.nav-menu').hasClass('is-active')){
+        $('.nav-menu').removeClass('is-active');
+        $('.nav-btn').removeClass('is-active');
     }else{
-        $('.nav1-menu').addClass('is-active');
-        $('.nav1-btn').addClass('is-active');
-    }
-});
-
-$('.nav2-btn,.nav2-bg').on('click',function(){
-    if($('.nav2-menu').hasClass('is-active')){
-        $('.nav2-menu').removeClass('is-active');
-    }else{
-        $('.nav2-menu').addClass('is-active');
+        $('.nav-menu').addClass('is-active');
+        $('.nav-btn').addClass('is-active');
     }
 });
